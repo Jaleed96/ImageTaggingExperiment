58544e7c7c0b4f400f6570fed43da378b6ddde13
Hysteresis <crystalloid1@gmail.com>
2019-10-06 19:49:42 -0700

    nacnn edit

--- keras/NACNNModelBuilder.py
+++ keras/NACNNModelBuilder.py
@@ -32,23 +32,26 @@ model = Sequential()
 model.add(layers.Conv2D(32, (3, 3), padding='same', input_shape=(None, None, 3)))
 model.add(layers.Activation('relu'))
 model.add(layers.MaxPooling2D(pool_size=(2, 2)))
-model.add(layers.GaussianNoise(0.25))
+model.add(layers.Dropout(0.25))
+model.add(layers.GaussianNoise(0.1))
 model.add(layers.Conv2D(64, (3, 3), padding='same'))
 model.add(layers.Activation('relu'))
 model.add(layers.MaxPooling2D(pool_size=(2, 2)))
-model.add(layers.GaussianNoise(0.25))
+model.add(layers.Dropout(0.25))
+model.add(layers.GaussianNoise(0.1))
 model.add(layers.Conv2D(128, (3, 3)))
 model.add(layers.Activation('relu'))
 model.add(SpatialPyramidPooling([1, 2, 4]))
-model.add(layers.GaussianNoise(0.5))
+model.add(layers.GaussianNoise(0.1))
 model.add(layers.Dense(512))
 model.add(layers.Activation('relu'))
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(100))
 model.add(layers.Activation('softmax'))
 model.summary()
 
 # opt = keras.optimizers.RMSprop(learning_rate=0.0001, decay=1e-6)
-learning_rate = 0.1
+learning_rate = 0.001
 epochs = 100
 decay_rate = learning_rate / epochs
 momentum = 0.9
@@ -57,7 +60,7 @@ model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 # model_path = os.path.join(dir_path, 'sppRMSprop.h5')
-model_path = os.path.join(dir_path, 'NACNN2.h5')
+model_path = os.path.join(dir_path, 'NACNN4.h5')
 if(os.path.exists(model_path)):
     model.load_weights(model_path)
 
